bb97478a51cf0cdafdbb5cc0222d468916c4cdcc
Ondrej Hrabina <hrabina.ondra@gmail.com>
2018-05-08 16:51:30 +0200

    if.py

---
+++ for.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    if letter in "AEIOU":
+        print(letter, "je samohláska")
+    else:
+        print(letter, "je souhláska")
+    
